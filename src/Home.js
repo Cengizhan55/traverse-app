35e834409fc37c1609a686125b1e36795b1d3d9b
Gökay Erez <gokayerez@hotmail.com>
2022-12-25 14:55:49 +0300

    landing background fix

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,12 @@ import './Home.css'
 
 const HomePage = () => { 
     return(
-        <div className='landing-left'>
-            <h1 className='landing-header'>View attractions, hotels and restaurants anywhere in the world!</h1>
-            <p className='landing-para'><b>Traverse</b> is a free and easy to use web application that uses <i>Google Maps</i> to easily show you nearby recommended attractions. Use the button to get started.</p>
-            <button className='landing-button'>Start</button>
+        <div className="landing-root">
+            <div className='landing-left'>
+                <h1 className='landing-header'>View attractions, hotels and restaurants anywhere in the world!</h1>
+                <p className='landing-para'><b>Traverse</b> is a free and easy to use web application that uses <i>Google Maps</i> to easily show you nearby recommended attractions. Use the button to get started.</p>
+                <button className='landing-button'>Start</button>
+            </div>
         </div>
     );
 }
